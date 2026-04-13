6d62ec719cae164df6b78e0ecf42e74f97a7adbd
alessio <alessio@imada.sdu.dk>
2026-04-13 16:41:15 +0200

    removed unused scripts

--- run_scripts/run_fzn2feat.sh
+++
@@ -1,5 +0,0 @@
-python src/algorithm_selection/algorithm_selection.py -f fzn2feat -m rnd-forest --cv-fold 0 --result results3/as_fzn2feat-forest-0.json
-python src/algorithm_selection/algorithm_selection.py -f fzn2feat -m rnd-forest --cv-fold 1 --result results3/as_fzn2feat-forest-1.json
-python src/algorithm_selection/algorithm_selection.py -f fzn2feat -m rnd-forest --cv-fold 2 --result results3/as_fzn2feat-forest-2.json
-python src/algorithm_selection/algorithm_selection.py -f fzn2feat -m rnd-forest --cv-fold 3 --result results3/as_fzn2feat-forest-3.json
-python src/algorithm_selection/algorithm_selection.py -f fzn2feat -m rnd-forest --cv-fold 4 --result results3/as_fzn2feat-forest-4.json
